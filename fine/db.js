b9c72fc5bcd41f7bcbcf1394daf92693c2d63700
jricica <janricica@ufm.edu>
2025-07-04 16:19:39 -0600

    db template

--- fine/db.js
+++ fine/db.js
@@ -1,9 +1,9 @@
-// fine/db.js
+
 import mysql from 'mysql2/promise';
 
 export const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Xapkib/Mriya2004', 
-  database: 'escuela',
+  host: '',
+  user: '',
+  password: '', 
+  database: '',
 });
