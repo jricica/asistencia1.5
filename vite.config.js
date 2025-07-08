1e361800763d121236100cfe86c4f6e474d798e5
jricica <janricica@ufm.edu>
2025-07-07 19:42:31 -0600

    publish

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  base: "/asistencia1.5/",
   plugins: [react()],
   resolve: {
     alias: {
@@ -12,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000", // ← puerto donde corre tu backend (server.js)
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
       },
