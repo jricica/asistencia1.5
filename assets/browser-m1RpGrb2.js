8ca4f8bb984d1d32d3fe4acf063df50703334728
jricica <janricica@ufm.edu>
2025-07-16 01:41:24 -0600

    Updates

---
+++ assets/browser-m1RpGrb2.js
@@ -0,0 +1,1 @@
+import{Headers as e,Request as t,Response as n,browser_default as r,fetch as i,init_browser as a}from"./index-BpSUn9-V.js";a();export{e as Headers,t as Request,n as Response,r as default,i as fetch};
